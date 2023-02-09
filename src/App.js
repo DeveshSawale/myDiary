0894da9525eead29097799b32ea8fe62fc7c3d08
Devesh Sawale <im007dev@gmail.com>
2023-02-09 22:51:12 +0530

    Added login and signup

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import {
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import About from "./components/About"
+import Login from "./components/Login"
+import Signup from "./components/Signup"
 import NoteState from './context/notes/NoteState';
 import Alert from './components/Alert';
 
@@ -21,6 +23,8 @@ function App() {
             <Routes>
               <Route exact path="/" element={<Home />} />
               <Route exact path="/about" element={<About />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/signup" element={<Signup />} />
             </Routes>
           </div>
         </Router>
